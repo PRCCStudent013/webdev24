cdc7d1fa80a48ec25d41b68b345ac470a99ac6db
PRCCStudent013 <155987948+PRCCStudent013@users.noreply.github.com>
2024-02-06 08:41:44 -0600

    client side chapter 2 update

--- clientSide/Chapter02/chapter/js02.js
+++ clientSide/Chapter02/chapter/js02.js
@@ -23,7 +23,43 @@ function setupForm()
 {
    document.getElementById("photoNum").value = 1;
    document.getElementById("photoHrs").value = 2;
+
    document.getElementById("makeBook").checked = false;
    document.getElementById("photoRights").checked = false;
    document.getElementById("photoDist").value = 0;
+
+   getEstimate();
+
+   //Add event handlers for each input control
+   document.getElementById("photoNum").onchange = getEstimate;
+   document.getElementById("photoHrs").onchange = getEstimate;
+   document.getElementById("photoDist").onchange = getEstimate;
+   document.getElementById("makeBook").onchange = getEstimate;
+   document.getElementById("photoRights").onchange = getEstimate;
 }
+
+// estimate the total cost of the service
+function getEstimate() {
+      let totalCost = 0;
+      let photographers = document.getElementById("photoNum").value;
+      let hours = document.getElementById("photoHrs").value;
+      let distance = document.getElementById("photoDist").value;
+      let buyBook = document.getElementById("makeBook").checked;
+      let buyRights = document.getElementById("photoRights").checked;
+
+      //Add the cost of photographers for hours covered
+      totalCost += photographers * hours * EMP_COST;
+
+      //Add the cost of distance per photographer per mile
+      totalCost += photographers * distance * TRAVEL_COST;
+
+      //Add the cost of the book if purchased
+      totalCost += buyBook ? BOOK_COST : 0;
+
+      //Add the cost of photo rights if purchased
+      totalCost += buyRights ? REPRO_COST : 0;
+
+      //Display the total cost estimate
+      document.getElementById("estimate").innerHTML = "$" + totalCost;
+}
+
