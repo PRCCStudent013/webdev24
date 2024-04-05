eac6e2267570145eaaea94373c1b98332b9e8f6e
PRCCStudent013 <155987948+PRCCStudent013@users.noreply.github.com>
2024-04-04 23:54:21 -0500

    Link modif

---
+++ clientSide/Chapter07/project03/project07-03_txt.js
@@ -0,0 +1,18 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 7
+      Project 07-03
+
+      Project to create a New Year's Eve countdown clock
+      Author: 
+      Date:   
+
+      Filename: project07-03.js
+*/
+
+let currentTime = document.getElementById("currentTime");
+let daysLeftBox = document.getElementById("days");
+let hrsLeftBox = document.getElementById("hours");
+let minsLeftBox = document.getElementById("minutes");
+let secsLeftBox = document.getElementById("seconds");
+
---
+++ clientSide/Chapter07/project04/project07-04_txt.js
@@ -0,0 +1,42 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 7
+      Project 07-04
+
+      Project to create a customer queue
+      Author: 
+      Date:   
+
+      Filename: project07-04.js
+*/
+
+let customers = ["Alisha Jordan","Kurt Cunningham", "Ricardo Lopez", "Chanda Rao",
+                 "Kevin Grant", "Thomas Bey", "Elizabeth Anderson", "Shirley Falk",
+                 "David Babin", "Arthur Blanding", "Brian Vick", "Jaime Aguilar",
+                 "Eileen Rios", "Gail Watts", "Margaret Wolfe", "Kathleen Newman",
+                 "Jason Searl", "Stephen Gross", "Robin Steinfeldt", "Jacob Bricker",
+                 "Gene Bearden", "Charles Sorensen", "John Hilton", "David Johnson",
+                 "Wesley Cho"];
+
+let customerName = document.getElementById("customerName");
+let customerList = document.getElementById("customerList");
+
+let addButton = document.getElementById("addButton");
+let searchButton = document.getElementById("searchButton");
+let removeButton = document.getElementById("removeButton");
+let topButton = document.getElementById("topButton");
+
+let status = document.getElementById("status");
+
+generateCustomerList();
+
+// Function to generate the ordered list based on the contents of the customers array
+function generateCustomerList() {
+   customerList.innerHTML = "";
+   for (let i = 0; i < customers.length; i++) {
+      let customerItem = document.createElement("li");      
+      customerItem.textContent = customers[i];     
+      customerList.appendChild(customerItem);
+   }
+}
+
