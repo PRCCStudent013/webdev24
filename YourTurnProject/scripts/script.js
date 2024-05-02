f3b71ab05fca682a412e2df27232cce2a94ebbd9
PRCCStudent013 <155987948+PRCCStudent013@users.noreply.github.com>
2024-05-01 23:51:21 -0500

    ...

--- YourTurnProject/scripts/script.js
+++ YourTurnProject/scripts/script.js
@@ -3,3 +3,13 @@
     File Name: script.js
     Date: 4/8/24
 */
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    var menuToggle = document.getElementById('menu-toggle');
+    var mobileNav = document.querySelector('.mobile-nav ul');
+
+    menuToggle.addEventListener('click', function() {
+        mobileNav.classList.toggle('show');
+    });
+});
